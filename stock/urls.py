73dfb66baa962593940d43005c2ae1d7d50b933b
codehunter <smagucha@gmail.com>
2021-01-16 14:06:55 +0300

    password resetting

--- stock/urls.py
+++ stock/urls.py
@@ -29,4 +29,13 @@ urlpatterns = [
     path('accounts/password_change/done/',
          auth_views.PasswordChangeDoneView.as_view(template_name='accounts/password_change_done.html'),
          name='password_change_done'),
+  
+    #path('accounts/password_reset/', auth_views.password_reset, name='password_reset'),
+    path('accounts/reset_password/', auth_views.PasswordResetView.as_view(template_name = "accounts/password_reset_form.html"), name ='reset_password'),
+    path('accounts/reset_password_sent/', auth_views.PasswordResetDoneView.as_view(template_name ='accounts/password_reset_done.html'),name='password_reset_done'),
+    path('accounts/reset/<uidb64>/<token>', auth_views.PasswordResetConfirmView.as_view(template_name ='accounts/password_reset_confirm.html'),),
+    path('accounts/reset_password_complete/', 
+        auth_views.PasswordResetCompleteView.as_view(template_name='accounts/password_reset_complete.html'),),
+
+  
 ]
