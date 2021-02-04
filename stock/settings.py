a3b13cffa322f3b2d9b8fc124cedbaa23eafa7f6
smagucha <smagucha@gmail.com>
2021-02-04 11:56:45 +0300

    adding postgresql

--- stock/settings.py
+++ stock/settings.py
@@ -77,14 +77,32 @@ WSGI_APPLICATION = 'stock.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
-
+#
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
 
+    'default': {
+
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+
+        'NAME': 'stockdatabase',
+
+        'USER': 'postgres',
+
+        'PASSWORD': 'm34sopAn!',
+
+        'HOST': 'localhost',
+
+        'PORT': '5432',
+
+    }
+
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/2.0/ref/settings/#auth-password-validators
